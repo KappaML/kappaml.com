45685d865188cb36d082b80008eef697d5b33f04
Alexandru-Ionut Imbrea <aleximbrea@gmail.com>
2022-01-09 21:56:18 +0100

    Fix some styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,13 +18,13 @@ function classNames(...classes) {
 const Navbar = class extends React.Component {
   render() {
     return (
-      <Disclosure as="nav" className="bg-gray-800" role="navigation" aria-label="main-navigation">
+      <Disclosure as="nav" role="navigation" aria-label="main-navigation">
         {({ open }) => (
           <>
             <div className="px-2 mx-auto sm:px-6 lg:px-8">
               <div className="relative flex items-center justify-between h-16">
                 <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                  <Disclosure.Button className="inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                     <span className="sr-only">Open main menu</span>
                     {open ? (
                       <XIcon className="block w-6 h-6" aria-hidden="true" />
@@ -36,18 +36,19 @@ const Navbar = class extends React.Component {
                 <div className="flex items-center justify-center flex-1 sm:items-stretch sm:justify-between">
                   <div className="flex items-center flex-shrink-0">
                     <Link to="/" title="Logo">
-                      <img src={logo} alt="KappaML" style={{ width: "100px" }} />
+                      <img src={logo} alt="KappaML" style={{ width: "130px" }} />
                     </Link>
                   </div>
-                  <div className="hidden sm:block sm:ml-6">
-                    <div className="flex space-x-4 flex-items-center">
+
+                  <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                    <div className="hidden space-x-4 sm:flex sm:mr-6 sm:border-r sm:pr-2">
                       {navigation.map((item) => (
                         <Link
                           key={item.name}
                           to={item.href}
                           className={classNames(
-                            item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                            'px-3 py-2 rounded-md text-sm font-medium'
+                            item.current ? 'bg-gray-900 text-white' : 'hover:bg-gray-700 hover:text-white',
+                            'px-3 py-2 rounded-md'
                           )}
                           aria-current={item.current ? 'page' : undefined}
                         >
@@ -55,10 +56,8 @@ const Navbar = class extends React.Component {
                         </Link>
                       ))}
                     </div>
-                  </div>
-                  <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                     <a
-                      className="w-8 h-8 text-gray-400 hover:text-white"
+                      className="w-8 h-8 hover:text-black"
                       href="https://github.com/KappaML"
                       target="_blank"
                       rel="noopener noreferrer"
@@ -80,7 +79,7 @@ const Navbar = class extends React.Component {
                     as="a"
                     href={item.href}
                     className={classNames(
-                      item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                      item.current ? 'bg-gray-900 text-white' : 'hover:bg-gray-700 hover:text-white',
                       'block px-3 py-2 rounded-md text-base font-medium'
                     )}
                     aria-current={item.current ? 'page' : undefined}
