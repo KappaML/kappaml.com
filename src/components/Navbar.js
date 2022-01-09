f43f2d99751f6d33cb9ff0712e0525781bfaf40b
Alexandru-Ionut Imbrea <aleximbrea@gmail.com>
2022-01-09 02:43:08 +0100

    Adjust navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,7 +22,7 @@ const Navbar = class extends React.Component {
       <Disclosure as="nav" className="bg-gray-800" role="navigation" aria-label="main-navigation">
         {({ open }) => (
           <>
-            <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+            <div className="mx-auto px-2 sm:px-6 lg:px-8">
               <div className="relative flex items-center justify-between h-16">
                 <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                   <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
@@ -37,7 +37,7 @@ const Navbar = class extends React.Component {
                 <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                   <div className="flex-shrink-0 flex items-center">
                     <Link to="/" title="Logo">
-                      <img src={logo} alt="KappaML" style={{ width: "88px" }} />
+                      <img src={logo} alt="KappaML" style={{ width: "100px" }} />
                     </Link>
                   </div>
                   <div className="hidden sm:block sm:ml-6">
