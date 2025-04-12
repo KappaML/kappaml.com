2f7941ed6e1a0bad3f86f66d7ce422d060c09169
Alex Imbrea <alex@kappaml.com>
2025-04-12 19:23:17 +0200

    Update Navbar to include a new logo and link to KappaML app

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 [![Netlify Status](https://api.netlify.com/api/v1/badges/c873bd2a-b743-46c0-8dcf-89b430e1b208/deploy-status)](https://app.netlify.com/sites/suspicious-keller-46a0e4/deploys)
 
 It follows the [JAMstack architecture](https://jamstack.org) by using Git as a single source of truth, and [Netlify](https://www.netlify.com) for continuous deployment, and CDN distribution.
+
+# Development
+
+```
+yarn install
+```
+
+```
+yarn develop
+```
+
+
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby";
 import { Disclosure } from '@headlessui/react'
 import { Bars3Icon as MenuIcon, XMarkIcon as XIcon } from '@heroicons/react/24/outline'
 import logo from "../img/logo.svg";
+import symbol from "../img/symbol-white.svg";
 
 const navigation = [
   { name: 'Contact', href: '/contact' },
@@ -55,15 +56,12 @@ const Navbar = class extends React.Component {
                       ))}
                     </div>
                     <a
-                      className="w-8 h-8 hover:text-black"
-                      href="https://github.com/KappaML"
+                      href="https://app.kappaml.com/login"
                       target="_blank"
                       rel="noopener noreferrer"
-                      aria-label="Visit KappaML GitHub"
+                      className="px-2 py-2 text-sm font-semibold text-white transition duration-300 ease-in-out rounded-lg bg-primary hover:bg-cyan-600 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2"
                     >
-                      <svg viewBox="0 0 16 16" fill="currentColor" aria-hidden="true">
-                        <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
-                      </svg>
+                      <img src={symbol} className="w-6 h-6" />
                     </a>
                   </div>
                 </div>
